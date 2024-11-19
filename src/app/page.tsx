b0445a92bd1427f87a1aa527ab5d36baa14dbb8b
juansubas <juansubas@gmail.com>
2024-11-19 01:19:59 -0500

    Update Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div >
-      <h1>Hello World</h1>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="bg-red-400">Hola somos Om Group, una ayuda a tus metas.</h1>
     </div>
   );
 }
